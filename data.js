c0a3402451aa0257d410d7e3f0d82cb0d599f27c
Sebastian Lees <sebastian@incerto.net>
2020-03-23 13:01:16 +0000

    ADDED Vue.js mechanics. EXPORTED data.

---
+++ data.js
@@ -0,0 +1,76 @@
+var data = {
+  "data": [
+    {
+      "title": "Centre for Complexity Science (Imperial College London)",
+      "category": "Research Institution",
+      "tags": [
+        "complexity",
+        "Univeristy",
+        "Research"
+      ],
+      "icon": "fa fa-graduation-cap mr-2",
+      "link": "https://www.imperial.ac.uk/complexity-science/",
+      "description": "The Centre engages in a range of research into the applied and fundamental aspects of complexity science, where often the most interesting questions extend beyond the realms of idealised models and the simplifications arising within typical experiments."
+    },
+    {
+      "title": "Centre for Complexity Science (Imperial College London)",
+      "category": "Research Institution",
+      "tags": [
+        "complexity",
+        "Univeristy",
+        "Research"
+      ],
+      "icon": "fa fa-user mr-2",
+      "link": "https://www.imperial.ac.uk/complexity-science/",
+      "description": "The Centre engages in a range of research into the applied and fundamental aspects of complexity science, where often the most interesting questions extend beyond the realms of idealised models and the simplifications arising within typical experiments."
+    },
+    {
+      "title": "Bristol Centre for Complexity Sciences (University of Bristol)",
+      "category": "Research Institution",
+      "tags": [
+        "complexity",
+        "Univeristy",
+        "Research"
+      ],
+      "icon": "fa fa-user mr-2",
+      "link": "http://www.bristol.ac.uk/bccs/",
+      "description": "The 'Complexity Complex' was established in 2005 with the goal of catalysing cross-disciplinary research in Complexity Science. It built on an ESRC-supported seminar series on 'Socio-economic dynamics', and two EC network projects co-ordinated from Warwick ('Unifying networks in science and society' and 'Complex financial markets') and the work of a number of research groups such as: The Centre for Fusion, Space & Astrophysics; Information Systems Research Unit; Centre for Primary Health Care Studies; and Systems Biology.   "
+    },
+    {
+      "title": "Centre for Complexity Science (Warwick University)",
+      "category": "Research Institution",
+      "tags": [
+        "complexity",
+        "Univeristy",
+        "Research"
+      ],
+      "icon": "fa fa-user mr-2",
+      "link": "https://warwick.ac.uk/fac/cross_fac/complexity/",
+      "description": "EPSRC Taught Courses in Complexity Science and Complex Systems EPSRC is funding a number of courses for interested researchers that are new to complexity science and complex systems. The courses, running in 2006, are for new researchers and for those with more experience who want to find out more about the underlying concepts in this emerging research area. The courses will also provide an opportunity for networking with researchers from complementary fields."
+    },
+    {
+      "title": "ONCE-CS (Cranfield University)",
+      "category": "Research Institution",
+      "tags": [
+        "complexity",
+        "Univeristy",
+        "Research"
+      ],
+      "icon": "fa fa-user mr-2",
+      "link": "http://www.casa.ucl.ac.uk/once-cs/epsrc_courses.htm",
+      "description": "<p>EPSRC Taught Courses in Complexity Science and Complex Systems EPSRC is funding a number of courses for interested researchers that are new to complexity science and complex systems. The courses, running in 2006, are for new researchers and for those with more experience who want to find out more about the underlying concepts in this emerging research area. The courses will also provide an opportunity for networking with researchers from complementary fields."
+    },
+    {
+      "title": "Institute of Complex Systems Simulation",
+      "category": "Research Institution",
+      "tags": [
+        "complexity",
+        "Univeristy",
+        "Research"
+      ],
+      "icon": "fa fa-user mr-2",
+      "link": "http://www.icss.soton.ac.uk/",
+      "description": "The Institute for Complex Systems Simulation (ICSS), of the University of Southampton, has concluded its doctoral training centre (DTC), and pleasingly, has fulfilled its mission to recruit, develop and graduate research leaders and pioneers of complexity science. Our PhD graduates are field specialists, able to analyse, innovate and exploit complex systems models and methodologies. Moreover, the thesis outputs spanned almost the full range of academic research in the university, from the study of flamingo feeding patterns in Kenya and climate dynamics in the Arctic, to the fundamental understanding of mechanical properties in biological membranes and the potential uses of micromagnetics in data storage devices. All projects were complexity focused, addressing the emerging behaviour from complex systems, with a significant simulation or mathematical component. Many of our projects had very direct societal relevance, including human sexual behaviour, while others addressed more fundamental but important physical principles, such as non-equilibrium processes in dynamic systems."
+    },
+  ]
+}
